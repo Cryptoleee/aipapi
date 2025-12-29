3ef6c2d9ab799fd4af4494a3db002992e09eefe2
Cryptoleee <163641871+Cryptoleee@users.noreply.github.com>
2025-12-29 10:44:26 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App';
+// @ts-ignore
+import App from './App.jsx';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
